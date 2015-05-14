ab271b3827512ccccd14a94b5d9fe56d354e569d
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-14 15:49:43 +0200

    Raise errors if ConfigKey or -File not found

--- performance_testing/command_line.py
+++ performance_testing/command_line.py
@@ -8,13 +8,18 @@ class Tool:
         self.create_result_directory(result_directory)
 
     def read_config(self, config):
-        config_stream = open(config, 'r')
-        config_data = yaml.load(config_stream)
-        self.host = config_data['host']
-        self.requests = config_data['requests']
-        self.clients = config_data['clients']
-        self.time = config_data['time']
-        self.urls = config_data['urls']
+        try:
+            config_stream = open(config, 'r')
+            config_data = yaml.load(config_stream)
+            self.host = config_data['host']
+            self.requests = config_data['requests']
+            self.clients = config_data['clients']
+            self.time = config_data['time']
+            self.urls = config_data['urls']
+        except KeyError, ex:
+            raise ex
+        except IOError:
+            raise IOError('Config file error "%s".' % config)
 
     def create_result_directory(self, directory):
         if not os.path.exists(directory):

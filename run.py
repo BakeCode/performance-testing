7ab12b7703221f39b3e53e6089c2640b67ebcb6b
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-15 11:32:12 +0200

    Bugfix false argument

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from performance_testing.command_line import Tool
 
 
 def main():
-    tool = Tool(config='config.yml', output_directory='result')
+    tool = Tool(config='config.yml', result_directory='result')
     tool.run()
 
 if __name__ == '__main__':

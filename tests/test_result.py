0f72371fae4f614932adc45a3461c1bcff5f569a
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-17 21:15:01 +0200

    Test for File.write_line()

--- tests/test_result.py
+++ tests/test_result.py
@@ -8,6 +8,8 @@ class ResultTestCase(unittest.TestCase):
     def setUp(self):
         self.current_directory = os.path.dirname(os.path.abspath(__file__))
         self.result_directory = os.path.join(self.current_directory, 'assets/test_result')
+        self.test_file_name = 'foo_bar'
+        self.test_file_path = os.path.join(self.result_directory, self.test_file_name)
 
     def clear_result_dir(self):
         if os.path.exists(self.result_directory):
@@ -21,11 +23,23 @@ class ResultTestCase(unittest.TestCase):
 
     def test_file_init(self):
         self.clear_result_dir()
-        file_name = 'foo_bar'
         self.assertFalse(os.path.exists(self.result_directory))
-        file = File(directory=self.result_directory, name=file_name)
+        file = File(directory=self.result_directory, name=self.test_file_name)
         self.assertTrue(os.path.exists(self.result_directory))
-        self.assertTrue(os.path.exists(os.path.join(self.result_directory, file_name)))
+        self.assertTrue(os.path.exists(self.test_file_path))
 
-    def tear_down(self):
+    def test_write_file(self):
+        self.clear_result_dir()
+        file = File(directory=self.result_directory, name=self.test_file_name)
+        stream = open(self.test_file_path, 'r')
+        self.assertEqual(stream.read(), '')
+        stream.close()
+
+        text = 'askld asjdjidjj saidj98e12ud0- asid902ur890a'
+        file.write_line(text)
+        stream = open(self.test_file_path, 'r')
+        self.assertEqual(stream.read(), text + '\n')
+        stream.close()
+
+    def tearDown(self):
         self.clear_result_dir()

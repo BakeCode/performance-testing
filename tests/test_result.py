4a4a3eed7b959e342e3ff00dfc28f116158839d6
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-17 21:04:47 +0200

    Split up tests for Result and File

--- tests/test_result.py
+++ tests/test_result.py
@@ -9,10 +9,23 @@ class ResultTestCase(unittest.TestCase):
         self.current_directory = os.path.dirname(os.path.abspath(__file__))
         self.result_directory = os.path.join(self.current_directory, 'assets/test_result')
 
-    def test_result_init(self):
+    def clear_result_dir(self):
         if os.path.exists(self.result_directory):
             shutil.rmtree(self.result_directory)
+
+    def test_result_init(self):
+        self.clear_result_dir()
         self.assertFalse(os.path.exists(self.result_directory))
         result = Result(directory=self.result_directory)
+        self.assertTrue(result.file.path)
+
+    def test_file_init(self):
+        self.clear_result_dir()
+        file_name = 'foo_bar'
+        self.assertFalse(os.path.exists(self.result_directory))
+        file = File(directory=self.result_directory, name=file_name)
         self.assertTrue(os.path.exists(self.result_directory))
-        self.assertTrue(os.path.exists(result.file.path))
+        self.assertTrue(os.path.exists(os.path.join(self.result_directory, file_name)))
+
+    def tear_down(self):
+        self.clear_result_dir()

8f70981f93da9648a618fb529629e5bdd88a1f7d
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-19 09:25:03 +0200

    Remove yaml config and use config object

--- tests/test_command_line_tool.py
+++ tests/test_command_line_tool.py
@@ -3,15 +3,15 @@ import os
 from performance_testing.command_line import Tool
 from performance_testing.result import Result
 from performance_testing.config import Config
+import config
 
 
 class CommandLineToolTest(unittest.TestCase):
     def setUp(self):
         self.current_directory = os.path.dirname(os.path.abspath(__file__))
-        self.config = os.path.join(self.current_directory, 'assets/test_config.yml')
         self.result_directory = os.path.join(self.current_directory, 'assets/test_result')
 
     def test_init(self):
-        tool = Tool(config=self.config, result_directory=self.result_directory)
+        tool = Tool(config=config.CONFIG, result_directory=self.result_directory)
         self.assertIsInstance(tool.config, Config)
         self.assertIsInstance(tool.result, Result)

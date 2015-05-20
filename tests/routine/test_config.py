016181fcbb810dbbadef34458e9a8de61ca82e09
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-20 20:32:17 +0200

    Write add_request test

--- tests/routine/test_config.py
+++ tests/routine/test_config.py
@@ -1,5 +1,6 @@
 import unittest
 from performance.routine import Config
+from performance.web import Request
 
 
 class ConfigTestCase(unittest.TestCase):
@@ -10,3 +11,14 @@ class ConfigTestCase(unittest.TestCase):
         config = Config(host=self.host)
         self.assertEqual(self.host, config.host)
         self.assertEqual([], config.requests)
+
+    def test_add_request(self):
+        config = Config(host=self.host)
+        requestA = Request(url='/', type=Request.GET)
+        requestB = Request(url='/about', type=Request.POST)
+        config.add_request(requestA)
+        config.add_request(requestB)
+        self.assertEqual([requestA, requestB], config.requests)
+        with self.assertRaises(TypeError) as error:
+            config.add_request('no_request_type')
+        self.assertEqual('No performance.web.Request object', error.exception.__str__())

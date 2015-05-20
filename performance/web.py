f7f3907da8ec31cafaec3d7e82755cfa344ed60e
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-20 19:46:01 +0200

    Create Error-classes, Client class and do function for Request

--- performance/web.py
+++ performance/web.py
@@ -1,3 +1,12 @@
+import requests
+from time import time
+
+
+class Client:
+    def __init__(self):
+        pass
+
+
 class Request:
     GET = 'get'
     POST = 'post'
@@ -7,6 +16,27 @@ class Request:
         self.type = type
         self.data = data
 
+    def do(self):
+        try:
+            data = ''
+            if isinstance(self.data, RequestData):
+                data = self.data.for_type(type=self.type)
+            self.started = time()
+            response = getattr(requests, self.type)(
+                url=self.url,
+                data=data
+            )
+            self.finished = time()
+            self.status_code = response.status_code
+        except AttributeError:
+            raise RequestTypeError(type=self.type)
+
+    def get_response_time(self):
+        try:
+            return self.finished - self.started
+        except AttributeError:
+            raise RequestTimeError
+
 
 class RequestData:
     def __init__(self, data=None):
@@ -15,3 +45,15 @@ class RequestData:
     def for_type(self, type=Request.GET):
         if type is Request.GET:
             return data
+
+
+class RequestTypeError(Exception):
+    def __init__(self, type):
+        self.type = type
+
+    def __str__(self):
+        return 'Invalid request type "%s".' % self.type
+
+
+class RequestTimeError(Exception):
+    pass

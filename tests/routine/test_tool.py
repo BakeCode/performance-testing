fb26bb8caf83fdf5638e1cac276599fc2da00ae9
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-22 10:08:55 +0200

    Update test for print-statements

--- tests/routine/test_tool.py
+++ tests/routine/test_tool.py
@@ -1,19 +1,39 @@
 import unittest
 from performance.routine import Tool, Config
+from performance.web import Request
+from io import StringIO
+import sys
 
 
 class ToolTestCase(unittest.TestCase):
     def setUp(self):
         self.host = 'http://www.google.com'
-        self.config = Config(host=self.host)
+        self.requests = []
+        self.requests.append(Request(url='/'))
+        self.requests.append(Request(url='/about'))
 
     def test_init(self):
-        tool = Tool(config=self.config)
-        self.assertEqual(self.config, tool.config)
+        config = Config(host=self.host)
+        tool = Tool(config=config)
+        self.assertEqual(config, tool.config)
         with self.assertRaises(TypeError) as error:
             tool = Tool(config='invalid_config')
         self.assertEqual('No performance.routine.Config object', error.exception.__str__())
 
     def test_run(self):
-        tool = Tool(config=self.config)
+        config = Config(host=self.host, clients_count=2)
+        print_results = StringIO()
+        sys.stdout = print_results
+        tool = Tool(config=config)
         tool.run()
+        self.assertEqual(' > Invalid configuration\n', print_results.getvalue())
+        print_results.close()
+        print_results = StringIO()
+        sys.stdout = print_results
+        config.add_request(self.requests[0])
+        config.add_request(self.requests[1])
+        tool = Tool(config=config)
+        tool.run()
+        self.assertRegexpMatches(print_results.getvalue(), ' > Started tests\\n > Stop tests with CTRL-C\\n( > Finished a client\\n){2} > Finished 40 tests in [0-9]{1,10}\.[0-9]{2} seconds\\n')
+
+

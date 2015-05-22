970158ddeeb1d5ca6ffdc5f7a3254dd9a66f6e76
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-22 08:57:02 +0200

    Update config-example host

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from performance.web import Request
 
 
 def main():
-    config = Config(host='http://www.google.com', requests_per_client=10, clients_count=3)
+    config = Config(host='http://www.example.com', requests_per_client=10, clients_count=3)
     config.add_request(Request(url='/'))
     tool = Tool(config=config)
     tool.run()

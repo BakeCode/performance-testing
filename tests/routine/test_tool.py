4c5d9866f3ec0fd570eaa2b3a4c073d9dd74c3cf
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-22 10:57:52 +0200

    Write test for CTRL-C interrupt

--- tests/routine/test_tool.py
+++ tests/routine/test_tool.py
@@ -3,6 +3,9 @@ from performance.routine import Tool, Config
 from performance.web import Request
 from io import StringIO
 import sys
+from threading import Thread
+import _thread
+import time
 
 
 class ToolTestCase(unittest.TestCase):
@@ -35,4 +38,20 @@ class ToolTestCase(unittest.TestCase):
         tool = Tool(config=config)
         tool.run()
         self.assertRegexpMatches(print_results.getvalue(), ' > Started tests\\n > Stop tests with CTRL-C\\n( > Finished a client\\n){2} > Finished 40 tests in [0-9]{1,10}\.[0-9]{2} seconds\\n')
+
+    def test_run_interrupt(self):
+        config = Config(host=self.host, clients_count=2)
+        print_results = StringIO()
+        sys.stdout = print_results
+        config.add_request(self.requests[0])
+        config.add_request(self.requests[1])
+        tool = Tool(config=config)
+        thread = Thread(target=interrupt)
+        thread.start()
+        tool.run()
+        self.assertEqual(' > Started tests\n > Stop tests with CTRL-C\n > Interrupted a client\n > Interrupted a client\n > Exited with CTRL-C\n', print_results.getvalue())
+
+def interrupt():
+    time.sleep(1)
+    _thread.interrupt_main()
 

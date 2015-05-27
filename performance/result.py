38fea0e69101109ab52e681b9b15e4929bd8d4cd
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-27 10:47:03 +0200

    Create Result class to save results in one object

---
+++ performance/result.py
@@ -0,0 +1,19 @@
+import Queue
+import json
+
+
+class Result:
+    def __init__(self):
+        self.results = {}
+
+    def add_result(self, client, url, result):
+        if client not in self.results:
+            self.results[client] = {}
+        if url not in self.results[client]:
+            self.results[client][url] = []
+        self.results[client][url].append(result)
+
+    def write(self, result_file):
+        stream = open(result_file, 'w')
+        stream.write(json.dumps(self.results))
+        stream.close()
